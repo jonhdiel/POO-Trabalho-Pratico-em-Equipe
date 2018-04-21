0fa74405a5255a3377ff993dab8741f255e62b8d
João Diel <jonhdiel@gmail.com>
2018-04-20 21:05:34 -0300

    Adicionados arquivos java das respostas do URI Judge

---
+++ 1805.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+		long res=0;
+		Scanner ler = new Scanner(System.in);
+		long A = Long.parseLong(ler.next());
+		long B = Long.parseLong(ler.next());
+		
+		while(A <= B){
+			res+=A;
+			A++;
+		}	
+		System.out.println(res);		
+    }
+ 
+}
+
+
+
+
+
